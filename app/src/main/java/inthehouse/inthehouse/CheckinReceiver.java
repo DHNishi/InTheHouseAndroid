ae39d01a988325e403e90e0df5fb4684f8f123d3
dhnishi <dhnishi@gmail.com>
2015-02-19 10:50:22 -0800

    Now with active network.

--- app/src/main/java/inthehouse/inthehouse/CheckinReceiver.java
+++ app/src/main/java/inthehouse/inthehouse/CheckinReceiver.java
@@ -29,7 +29,12 @@ public class CheckinReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         ConnectivityManager connMgr =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        //NetworkInfo netInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
+
+        if (netInfo != null && !netInfo.getTypeName().equalsIgnoreCase("WIFI")) {
+            return;
+        }
 
         if (netInfo != null) {
             if (netInfo.isConnected()) {
